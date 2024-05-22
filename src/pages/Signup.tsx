71bc3474ea4d67a6b435e692e0cbdaad8e6ee355
Amadeja <131427620+amadejaop@users.noreply.github.com>
2024-05-22 18:51:13 +0200

    Add basic structure of the signup page

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,7 +1,35 @@
+import BoldLink from '../components/BoldLink'
+
 export default function Signup() {
     return (
         <>
-          <h1>Signup</h1>
+          <div>
+            <img src="" alt="Auction cards screenshot" />
+            <div>
+              <img src="" alt="Company logo" />
+              <h2>Hello!</h2>
+              <p>Please enter your details</p>
+              <form action="post">
+                <label htmlFor="name">Name
+                  <input type="text" name="name" id="name" maxLength={20} />
+                </label>
+                <label htmlFor="surname">Surname
+                  <input type="text" name="surname" id="surname" maxLength={20} />
+                </label>
+                <label htmlFor="email">E-mail
+                  <input type="email" name="email" id="email" />
+                </label>
+                <label htmlFor="password">Password
+                  <input type="password" name="password" id="password" pattern="/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/" />
+                </label>
+                <label htmlFor="confirmPassword">Repeat password
+                  <input type="password" name="confirmPassword" id="confirmPassword" pattern="/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/" />
+                </label>
+                <button type="submit">Sign up</button>
+              </form>
+            </div>
+            <p>Already have an account? <BoldLink text={"Log in"} href={"/login"} /></p>
+          </div>
         </>
     )
 }
