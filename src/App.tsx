aa9ff86d2d7c044d14978940b3373610f6c35b43
Amadeja <131427620+amadejaop@users.noreply.github.com>
2024-05-23 17:08:54 +0200

    Add basic structure of the reset password page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import ResetPassword from "./pages/ResetPassword";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/ResetPassword.tsx
@@ -0,0 +1,36 @@
+import SideImage from "../components/SideImage";
+import Logo from "../assets/images/Logo.png";
+
+export default function ResetPassword() {
+  return (
+    <>
+      <div className="container">
+        <SideImage />
+        <div className="pw-section">
+          <img className="yellow-logo" src={Logo} alt="Company logo" />
+          <div className="inner-section">
+            <h2>Forgot password?</h2>
+            <p className="signup-para">
+              No worries, we will send you reset insturctions
+            </p>
+            <form action="post">
+              <label htmlFor="email">
+                E-mail
+                <input
+                  placeholder="Placeholder"
+                  className="signup-input"
+                  type="email"
+                  name="email"
+                  id="email"
+                />
+              </label>
+              <button type="submit" className="yellow-submit-btn">
+                Reset password
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
