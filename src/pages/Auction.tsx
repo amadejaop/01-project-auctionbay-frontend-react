b59579b18d0312433f69353b5bbb8325acb3364a
Amadeja <131427620+amadejaop@users.noreply.github.com>
2024-06-04 07:30:22 +0200

    Finish auction page for the item

--- src/pages/Auction.tsx
+++ src/pages/Auction.tsx
@@ -3,8 +3,34 @@ import Chair from "../assets/images/chair.jpeg";
 import "../assets/styles/Auction.css";
 import OutbidLg from "../components/tags/OutbidLg";
 import ClockLg from "../components/tags/ClockLg";
+import User1 from "../assets/images/user1.jpeg";
+import User2 from "../assets/images/user2.jpeg";
+import User from "../assets/images/Avatar.png";
+import { useState } from "react";
 
 export default function Auction() {
+  const [isShown, setIsShown] = useState(false);
+  const [price, setPrice] = useState(0);
+  const [dateNow, setDateNow] = useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const priceInt = parseInt(event.target.value);
+    setPrice(priceInt);
+  };
+
+  const handleSubmit = (event: React.MouseEvent<HTMLElement>) => {
+    event.preventDefault();
+    let time = new Date();
+    let hours = time.getHours() + 2;
+    let minutes = time.getMinutes();
+    let day = time.getDate();
+    let month = time.getMonth() + 1;
+    let year = time.getFullYear();
+    setDateNow(hours + ":" + minutes + " " + day + "." + month + "." + year);
+
+    setIsShown(true);
+  };
+
   return (
     <>
       <TopNavigation />
@@ -30,15 +56,58 @@ export default function Auction() {
             Tempora veniam assumenda iusto expedita.
           </p>
           <form action="">
-            <label htmlFor="bid">
+            <label htmlFor="price">
               Bid
-              <input type="number" placeholder="40" />
+              <input
+                type="number"
+                placeholder="40"
+                value={price}
+                id="price"
+                name="price"
+                onChange={handleChange}
+              />
             </label>
-            <button type="submit">Place bid</button>
+            <button type="submit" onClick={handleSubmit}>
+              Place bid
+            </button>
           </form>
         </div>
         <div className="item-bidding">
-          <h3>Bidding history</h3>
+          <h3>Bidding history({isShown ? "3" : "2"})</h3>
+          {isShown && (
+            <div className="user-bid">
+              <div className="user-info">
+                <img src={User} alt="user avatar" width={32} height={32} />
+                <p className="bid-username">Jamal Reces</p>
+              </div>
+              <div className="bid-date-price">
+                <p className="bid-date">{dateNow}</p>
+                <p className="bid-price">{price} €</p>
+              </div>
+            </div>
+          )}
+
+          <div className="user-bid">
+            <div className="user-info">
+              <img src={User1} alt="user avatar" width={32} height={32} />
+              <p className="bid-username">Joanes Boyl</p>
+            </div>
+            <div className="bid-date-price">
+              <p className="bid-date">14:31 22.6.2023</p>
+              <p className="bid-price">32 €</p>
+            </div>
+          </div>
+
+          <div className="user-bid">
+            <div className="user-info">
+              <img src={User2} alt="user avatar" width={32} height={32} />
+              <p className="bid-username">Bigi Smols</p>
+            </div>
+            <div className="bid-date-price">
+              <p className="bid-date">14:31 22.6.2023</p>
+              <p className="bid-price">29 €</p>
+            </div>
+          </div>
         </div>
       </div>
     </>
