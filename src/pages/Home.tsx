34102d6c5b4d1695c4381c4536edd58e02c9303a
Amadeja <131427620+amadejaop@users.noreply.github.com>
2024-06-20 05:44:49 +0200

    Create home page for mobile

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,20 @@ import Logo from "../assets/images/Logo.png";
 import BlackBgLink from "../components/BlackBgLink";
 import "../assets/styles/Home.css";
 import Auction from "../assets/images/Auctions1.png";
+import { useEffect, useState } from "react";
+import Screenshot from "../assets/images/screenshot.png";
 
 export default function Home() {
+  const [width, setWidth] = useState(window.innerWidth);
+  const breakpoint = 750;
+  useEffect(() => {
+   const handleResizeWindow = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResizeWindow);
+    return () => {
+      window.removeEventListener("resize", handleResizeWindow);
+    };
+  }, []);
+
   return (
     <>
       <div className="landing-page">
@@ -25,11 +37,15 @@ export default function Home() {
           </p>
           <YellowBgLink text={"Start bidding"} href={"/login"} />
         </div>
-        <img
+        {(width > breakpoint) ? <img
           src={Auction}
           alt="Screenshot from our app"
           className="landing-image"
-        />
+        /> : (
+          <div className="ss-container">
+            <img src={Screenshot} alt="Screenshot form our app" />
+          </div>
+        )}
       </div>
     </>
   );
